63047b4abbfbaf6255fd3060ca00128637cfc6c1
alyssarusk <83485098+alyssarusk@users.noreply.github.com>
2025-03-08 10:38:54 -0800

    Added js variables

--- gallery_script.js
+++ gallery_script.js
@@ -3,3 +3,9 @@ const imageGallery = [
     "https://images.pexels.com/photos/2416602/pexels-photo-2416602.jpeg?auto=compress&cs=tinysrgb&w=1200",
     "https://images.pexels.com/photos/2782485/pexels-photo-2782485.jpeg?auto=compress&cs=tinysrgb&w=1200"
 ]
+
+const imageElement = document.getElementById("gallery-image");
+const prevButton = document.getElementById("prev-btn");
+const nextButton = document.getElementById("next-btn");
+
+let currentImgIndex = 0;
