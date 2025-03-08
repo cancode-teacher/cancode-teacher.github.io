7ecba814ec8e8cc560e79cf3b3cbc980f1da2b28
alyssarusk <83485098+alyssarusk@users.noreply.github.com>
2025-03-08 11:05:33 -0800

    Added button script

--- gallery_script.js
+++ gallery_script.js
@@ -9,3 +9,19 @@ const prevButton = document.getElementById("prev-btn");
 const nextButton = document.getElementById("next-btn");
 
 let currentImgIndex = 0;
+
+function updateImage() {
+    imageElement.src = imageGallery[currentImgIndex];
+}
+
+nextButton.addEventListener("click", function(){
+    currentImgIndex = (currentImgIndex + 1) % imageGallery.length
+    updateImage()
+});
+
+prevButton.addEventListener("click", function(){
+    currentImgIndex = (currentImgIndex - 1 + imageGallery.length) % imageGallery.length
+    updateImage()
+});
+
+updateImage();
