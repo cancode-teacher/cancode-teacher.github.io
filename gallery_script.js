028969b77910d7383c091b19e4509bbb0d639b7a
cancode-teacher <cancode_prog@sfu.ca>
2026-02-26 18:57:02 -0800

    Added W5 code

--- gallery_script.js
+++ gallery_script.js
@@ -1,5 +1,22 @@
 const imageGallery = [
-    "link",
-    "link",
-    "link",
-]
+    "https://images.pexels.com/photos/11557791/pexels-photo-11557791.jpeg",
+    "https://images.pexels.com/photos/11424359/pexels-photo-11424359.jpeg",
+    "https://images.pexels.com/photos/1630883/pexels-photo-1630883.jpeg"
+];
+
+const imageElement = document.getElementById('gallery-image');
+const prevButton = document.getElementById('prev-btn');
+const nextButton = document.getElementById('next-btn');
+
+let currentImageIndex = 0;
+
+function updateImage() {
+    imageElement.src = imageGallery[currentImageIndex];
+}
+
+updateImage();
+
+nextButton.addEventListener('click', function() {
+    currentImageIndex = (currentImageIndex + 1) % imageGallery.length;
+    updateImage();
+});
